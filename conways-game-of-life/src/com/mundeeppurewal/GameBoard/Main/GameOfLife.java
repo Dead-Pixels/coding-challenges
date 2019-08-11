ccd88ba7f006a8050f123d2612ec535633ddda85
mundeeppurewal <mundeep.purewal@gmail.com>
2019-08-11 18:10:16 +0100

    Added function to update population state of the game for a particular living or dead being based on the game rule set

--- conways-game-of-life/src/com/mundeeppurewal/GameBoard/Main/GameOfLife.java
+++ conways-game-of-life/src/com/mundeeppurewal/GameBoard/Main/GameOfLife.java
@@ -4,19 +4,39 @@ public class GameOfLife {
 
     private String gameBoard[][];
 
-    public GameOfLife(){
+    public GameOfLife() {
     }
 
-    public GameOfLife(String gameboard[][]){
+    public GameOfLife(String gameboard[][]) {
         this.gameBoard = gameboard;
     }
 
     public void updatePopulationState() {
-        //Rules
 
+        for (int y = 0; y < gameBoard.length - 1; y++) {
+            for (int x = 0; x < gameBoard.length - 1; x++) {
+                // Any live cell with fewer than two live neighbors dies, as if caused by under population.
+                if (findNeighbours(gameBoard, y, x) < 2) {
+                    gameBoard[y][x] = "-";
+                }
+                // Any live cell with two or three live neighbors lives on to the next generation.
+                else if ((findNeighbours(gameBoard, y, x) == 2) || (findNeighbours(gameBoard, y, x) == 2)) {
+                    gameBoard[y][x] = "O";
+                }
+                // Any live cell with more than three live neighbors dies, as if by overpopulation.
+                else if ((findNeighbours(gameBoard, y, x) > 3) && (gameBoard[y][x] == "O")) {
+                    gameBoard[y][x] = "-";
+                }
+                // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+                else if ((findNeighbours(gameBoard, y, x) > 3) && (gameBoard[y][x] == "-")) {
+                    gameBoard[y][x] = "O";
+                }
+            }
+        }
     }
 
-    public void findNeighbours() {
+    public int findNeighbours(String gameBoard[][], int y, int x) {
+
 
     }
 
